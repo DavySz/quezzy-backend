025a2422c0fbe7f9d1f6cf0ba18756bbb763a03f
Davy Assunção <davy.assuncao@fretebras.com.br>
2024-10-12 19:25:46 -0300

    chore: initial project config

---
+++ src/test/java/com/davy/quezzy/QuezzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davy.quezzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuezzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
